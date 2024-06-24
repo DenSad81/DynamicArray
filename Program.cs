38419782da96185d4e1d4165dba9e486ceba63f5
DenSad81 <tagil81@mail.ru>
2024-06-24 10:29:34 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -8,34 +8,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        const string Sum = "sum";
-        const string Exit = "exit";
-        const string Print = "print";
-        bool isCiklWork = true;
+        const string CommandSum = "sum";
+        const string CommandExit = "exit";
+        const string CommandPrint = "print";
+
+        bool isWork = true;
         int[] array = new int[0];
-        string dataFromUser;
-        int pozitionInArray = 0;
+        string userInput;
 
-        while (isCiklWork)
+        while (isWork)
         {
-            Console.Write($"Введите число или команду {Sum} или команду {Exit} или команду {Print}: ");
-            dataFromUser = Console.ReadLine();
+            Console.Write($"Введите число или команду {CommandSum} или команду {CommandExit} или команду {CommandPrint}: ");
+            userInput = Console.ReadLine();
 
-            switch (dataFromUser)
+            switch (userInput)
             {
-                case Exit:
-                    isCiklWork = false;
+                case CommandExit:
+                    isWork = false;
                     break;
 
-                case Sum:
+                case CommandSum:
                     int sumOfArray = 0;
+
                     for (int i = 0; i < array.Length; i++)
                         sumOfArray += array[i];
 
                     Console.WriteLine($"Сумма членов массива: {sumOfArray}");
                     break;
 
-                case Print:
+                case CommandPrint:
+
                     for (int i = 0; i < array.Length; i++)
                         Console.WriteLine(array[i]);
 
@@ -43,12 +45,12 @@ class Program
 
                 default:
                     int[] tempArray = new int[array.Length + 1];
+
                     for (int i = 0; i < array.Length; i++)
                         tempArray[i] = array[i];
 
                     array = tempArray;
-                    array[pozitionInArray] = Convert.ToInt32(dataFromUser);
-                    pozitionInArray++;
+                    array[array.Length - 1] = Convert.ToInt32(userInput);
                     break;
             }
         }
